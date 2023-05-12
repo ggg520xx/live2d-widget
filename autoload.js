94e3b848b1c166b1c99bdf07393875043ee80a30
Kaya <ggg520xx@gmail.com>
2023-05-12 22:48:33 +0800

    commit push

--- autoload.js
+++ autoload.js
@@ -1,5 +1,5 @@
 // live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/ggg520xx/live2d-widget/";
+const live2d_path = "https://fastly.jsdelivr.net/gh/ggg520xx/live2d-widget/@new";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -34,9 +34,9 @@ if (screen.width >= 768) {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
+			// apiPath: "https://live2d.fghrsh.net/api/",
 			// apiPath: "https://fastly.jsdelivr.net/gh/ggg520xx/live2d-widget/waifu-tips.json",
-			// cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
