e40ffdc9b31319a7d5d914172a9b72d176733ac1
Kaya <ggg520xx@gmail.com>
2023-05-13 20:15:19 +0800

    use ai tag

--- autoload.js
+++ autoload.js
@@ -2,7 +2,7 @@
 const live2d_path = "https://fastly.jsdelivr.net/gh/ggg520xx/live2d-widget/";
 //const live2d_path = "/live2d-widget/";
 
-// ai tag
+// ai tag ai1
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
